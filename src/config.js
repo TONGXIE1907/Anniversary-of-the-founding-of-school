ae8f8550b952368eea9a798e79a6d1a16ccf5d98
TONGXIE1907 <130266315+TONGXIE1907@users.noreply.github.com>
2024-05-24 15:18:10 +0800

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,125 @@
+export default {
+  // page1部分
+  page1: {
+    titleEn: 'Hello,I`m ZhengRuiBin', //英文标题
+    title: '一个追梦在路上的小白', //中文标题
+  },
+  // page2部分
+  page2: {
+    authorImg: 'touxiang.jpg', // 作者头像
+    xinhui: '是一名在读的大二学生。', // 幸会
+    qiuzhi: '计算机实习', // 求职意向
+    guanyuwo: '始终保持对专业知识的热爱与追求' // 关于我
+  },
+  // page3部分
+  page3: [{
+    icon: 'icon-tubiao-',
+    title: '计算机知识',
+    msg: ['熟练使用计算机软件']
+  },{
+    icon: 'icon-diannao', // 图标
+    title: '网页制作', // 标题
+    msg: ['部署静态网页',] //介绍
+  }, {
+    icon: 'icon-qianbi1',
+    title: '博客撰写',
+    msg: ['运营CSDN、稀土掘金等平台']
+  },  {
+    icon: 'icon-shouji',
+    title: '微信公众号',
+    msg: ['',]
+  }],
+  // page4部分
+  page4: {
+    // 我的历程
+    course: [{
+      date: '', // 时间
+      desc: { // 经历
+        title: '学习表现',
+        list: ['始终保持对专业知识的热爱与追求，学习认真，成绩优异', '', '']
+      }
+    }, {
+      date: '2022/9——至今',
+      desc: {
+        title: '党课学习与⼊党积极分⼦',
+        list: ['积极参与党课学习，系统学习党的理论知识，不断提升⾃⼰的政治觉悟。']
+      }
+    }, {
+      date: '',
+      desc: {
+        title: '⼤创项⽬负责⼈',
+        list: ['担任[群众权益维护平台]的负责⼈，带领团队开展项⽬研究，积极与导师沟通，确保项⽬按照计划顺利进⾏，提升实践能⼒和团队协作能⼒。']
+      }
+    }],
+    // 我的拓展技能掌握
+    singlelist: [{
+      title: 'python',
+      text: '了解'
+    }, {
+      title: 'c.mysql',
+      text: '了解'
+    }, {
+      title: 'html.css.js',
+      text: '掌握'
+    }, {
+      title: 'office.wps',
+      text: '掌握'
+    }],
+    // 我的基本技能掌握
+    proresslist: [{
+      title: 'HTML、CSS',
+      value: '80%'
+    }, {
+      title: 'c,python',
+      value: '50%'
+    }, 
+    {
+      title: 'ps,wps,office',
+      value: '80%'
+    }, {
+      title: 'sql',
+      value: '80%'
+    }]
+  },
+  // page5部分
+  page5: [{
+    title: 'csdn主页',
+    content: '分享计算机相关的知识和见解',
+    image: 'box1.png',
+    href: 'https://blog.csdn.net/a871923942?spm=1000.2115.3001.5343'
+  }, {
+    title: '微信公众号',
+    content: '分享计算机相关的知识和见解',
+    image: 'box3.jpg',
+    href: 'https://mp.weixin.qq.com/s?__biz=Mzg4NDkyMDg5Mg==&mid=2247484287&idx=1&sn=055d23987a9f63164e4025f4e18ce6bf&chksm=cfb1842cf8c60d3ae4a9b0aca2873775ef9a2d589c12db0a29fe8afb1c1768ba03a948c5b873#rd'
+  }, {
+    title: '500px',
+    content: '日常摄影照片分享',
+    image: 'box5.jpg',
+    href: 'https://500px.com.cn/community/user-details/50ecfa7df46468cb92e69fd61179f5729'
+  }, {
+    title: '',
+    content: '',
+    image: 'box2.png',
+    
+  }, {
+    title: '个人简历',
+    content: '部署搭建个人简历网站',
+    image: 'box4.png',
+    href: ''
+  }, {
+    title: '',
+    content: '',
+    image: 'box6.jpeg',
+    href: ''
+  }],
+  // page6部分
+  page6: {
+    github: 'https://github.com/wttAndroid',
+    bili: '',
+    email: 'Mailto:1457321681@qq.com?Subject=邮箱标题&Body=邮箱内容！',
+    zhihu: '',
+    weixin: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/weix.jpg' alt='加载失败'>",
+    qq: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/qq.jpg' alt='加载失败'>"
+  }
+}
